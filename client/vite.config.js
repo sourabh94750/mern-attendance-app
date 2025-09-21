8c8439492ad420ae3adb8dbc26186fa3a80c4e42
SOURABH KARMAKAR <karmakarsourabh4321@gmail.com>
2025-09-21 22:27:36 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/mern-attendance-app"
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
